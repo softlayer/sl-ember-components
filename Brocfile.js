1957db514a061bfcad7f10c8c9a9b0f6c424df2d
Joshua Forisha <jforisha@softlayer.com>
2014-06-11 16:22:56 -0500

    Added more EmberUI-based components

--- Brocfile.js
+++ Brocfile.js
@@ -1,9 +1,40 @@
-var filterES6Modules = require('broccoli-es6-module-filter');
-var pickFiles = require('broccoli-static-compiler');
+var concat           = require( 'broccoli-concat' );
+var filterES6Modules = require( 'broccoli-es6-module-filter' );
+var filterTemplates  = require( 'broccoli-template' );
+var mergeTrees       = require( 'broccoli-merge-trees' );
+var pickFiles        = require( 'broccoli-static-compiler' );
+var templateCompiler = require( 'broccoli-ember-hbs-template-compiler' );
 
-module.exports = filterES6Modules(pickFiles('lib', {
-    srcDir: '/',
-    destDir: '/'
-  }), {
-    moduleType: 'amd'
-  });
+var scripts = pickFiles( 'lib', {
+    destDir: '/',
+    files:   [ '**/*.js' ],
+    srcDir:  '/'
+});
+
+var templates = templateCompiler( pickFiles( 'lib', {
+    destDir: '/templates',
+    files:   [ '**/*.hbs' ],
+    srcDir:  '/templates'
+}), {
+    module: true
+});
+
+function filter( tree ) {
+    return filterES6Modules( tree, {
+        anonymous:   false,
+        compatFix:   true,
+        main:        'main',
+        moduleType:  'amd',
+        packageName: 'interface-components'
+    });
+}
+
+module.exports = concat(
+    mergeTrees([
+        filter( templates ),
+        filter( scripts )
+    ]), {
+        inputFiles: [ '**/*.js' ],
+        outputFile: '/interface-components.js'
+    }
+);
