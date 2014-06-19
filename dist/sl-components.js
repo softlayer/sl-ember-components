68259412c09edb753f0ab0946cc3a2abc26708ee
Joshua Forisha <jforisha@softlayer.com>
2014-06-19 15:18:10 -0500

    Added sl-tabpanel active tab handling

--- dist/sl-components.js
+++ dist/sl-components.js
@@ -104,11 +104,68 @@ define("sl-components/components/sl-tabpanel",
      * and TabsPanesComponent into a single custom component.
      */
     __exports__["default"] = Ember.Component.extend({
+        actions: {
+            /**
+             * Changes the active tab to the tab with the specified name.
+             *
+             * @param {String} tabName - Name of the tab to make active
+             */
+            changeTab: function ( tabName ) {
+                var activeTabName = this.get( 'activeTabName' ),
+                    targetTab     = this.$( 'a.tab[data-tab-name="' + tabName + '"]' ),
+                    targetPane    = this.$( '.tab-pane[data-tab-name="' + tabName + '"]' );
+
+                if ( activeTabName ) {
+                    this.get( 'activeTab' ).removeClass( 'active' );
+                    this.get( 'activePane' ).removeClass( 'active' );
+
+                    if ( activeTabName === tabName ) {
+                        // Close the tab panel when the active tab is clicked again
+                        return this.close();
+                    }
+                }
+
+                targetTab.addClass( 'active' );
+                targetPane.addClass( 'active' );
+                this.set( 'activeTabName', tabName );
+            }
+        },
 
         /**
-         * Collects "title" attributes from content value to form column names
+         * Returns the tab pane that is active, as determined by activeTabName attr
+         *
+         * @returns {Object|null} The currently active tab pane
          */
-        columns: function () {
+        activePane: function () {
+            return this.$( '.tab-pane[data-tab-name="' + this.get( 'activeTabName' ) + '"]' );
+        }.property( 'activeTabName' ),
+
+        /**
+         * Returns the tab that is active, as determined by activeTabName attr
+         *
+         * @returns {Object|null} The currently active tab
+         */
+        activeTab: function () {
+            return this.$(' .tab[data-tab-name="' + this.get( 'activeTabName' ) + '"]' );
+        }.property( 'activeTabName' ),
+
+        activeTabName: null,
+
+        /**
+         * Closes the currently active tab pane and clears the active selection
+         */
+        close: function () {
+            this.get( 'activePane' ).removeClass( 'active' );
+            this.get( 'activeTab' ).removeClass( 'active' );
+            this.set( 'activeTabPane', null );
+        },
+
+        /**
+         * Collects "title" attributes from content value to form tab titles
+         *
+         * @returns {Array} Collection of tab titles
+         */
+        tabTitles: function () {
             return this.get( 'content' ).map( function ( item ) {
                 return item.title;
             });
@@ -713,7 +770,7 @@ define("sl-components/templates/sl-tabpanel",
 
 
       data.buffer.push(escapeExpression((helper = helpers['bs-tabs'] || (depth0 && depth0['bs-tabs']),options={hash:{
-        'contentBinding': ("columns"),
+        'contentBinding': ("tabTitles"),
         'selectedBinding': ("selected")
       },hashTypes:{'contentBinding': "STRING",'selectedBinding': "ID"},hashContexts:{'contentBinding': depth0,'selectedBinding': depth0},contexts:[],types:[],data:data},helper ? helper.call(depth0, options) : helperMissing.call(depth0, "bs-tabs", options))));
       data.buffer.push("\n\n");
