7a3be670a0d94161af02373c1f17f683e151f8bb
Joshua Forisha <jforisha@softlayer.com>
2014-07-16 15:57:16 -0500

    Added handling of initial and updated date content

--- app/components/sl-calendar.js
+++ app/components/sl-calendar.js
@@ -14,10 +14,31 @@ export default Ember.Component.extend( DateHandler, {
     classNames: [ 'sl-calendar' ],
 
     /**
+     * Which days of the week to disable
+     * @property {array} daysOfWeekDisabled - 0 (Sunday) to 6 (Saturday)
+     * @default [0, 1, 2, 3, 4, 5, 6]
+     */
+    daysOfWeekDisabled: [ 0, 1, 2, 3, 4, 5, 6 ],
+
+    /**
      * Setup for initial component state
      * @method didInsertElement
      */
     didInsertElement: function () {
         this.$().datepicker( this.get( 'options' ));
-    }
+        this.updateDates();
+    },
+
+    /**
+     * Updates the datepicker plugin's highlighted dates
+     * @method updateDates
+     */
+    updateDates: Ember.observer([ 'content', 'dateValuePath' ], function () {
+        var dateValuePath = this.get( 'dateValuePath' );
+        var dates = this.get( 'content' ).map( function ( item ) {
+            return moment( new Date( item[ dateValuePath ])).format( 'MM/DD/YYYY' );
+        });
+
+        this.$().datepicker( 'setDates', dates );
+    })
 });
