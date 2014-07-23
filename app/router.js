7ee5f410820140a73b673debb66ed36a3ce47857
Joshua Forisha <jforisha@softlayer.com>
2014-07-23 16:01:08 -0500

    Create sl-alert page and page index

--- app/router.js
+++ app/router.js
@@ -6,6 +6,22 @@ var Router = Ember.Router.extend({
 
 Router.map( function () {
     this.route( 'index', { path: '/' });
+
+    this.route( 'sl-alert' );
+    this.route( 'sl-button' );
+    this.route( 'sl-calendar' );
+    this.route( 'sl-chart' );
+    this.route( 'sl-checkbox' );
+    this.route( 'sl-datepicker' );
+    this.route( 'sl-dropbutton' );
+    this.route( 'sl-grid' );
+    this.route( 'sl-input' );
+    this.route( 'sl-radiogroup' );
+    this.route( 'sl-select' );
+    this.route( 'sl-simplemodal' );
+    this.route( 'sl-tabpanel' );
+    this.route( 'sl-textarea' );
+    this.route( 'sl-tooltip' );
 });
 
 export default Router;
