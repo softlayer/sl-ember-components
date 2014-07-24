c1473c879d3c2d7aead593433c03be7da50883f9
Joshua Forisha <jforisha@softlayer.com>
2014-07-24 10:02:22 -0500

    Add sl-chart demo page

---
+++ app/controllers/sl-chart.js
@@ -0,0 +1,33 @@
+import Ember from 'ember';
+
+export default Ember.ArrayController.extend({
+    actions: {
+        logOptions: function () {
+            console.log( this.get( 'chartOptions' ));
+        },
+
+        logSeries: function () {
+            console.log( this.get( 'content' ));
+        }
+    },
+
+    chartOptions: {
+        chart: {
+            type: 'bar'
+        },
+
+        title: {
+            text: 'Fruit Consumption'
+        },
+
+        xAxis: {
+            categories: [ 'Apples', 'Bananas', 'Oranges' ]
+        },
+
+        yAxis: {
+            title: {
+                text: 'Fruit Eaten'
+            }
+        }
+    }
+});
