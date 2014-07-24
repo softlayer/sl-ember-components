8ad31ec93e52df932c2c659f334bbe244179fcb4
Joshua Forisha <jforisha@softlayer.com>
2014-07-24 15:21:09 -0500

    Add label and helpText functionality

--- app/components/sl-textarea.js
+++ app/components/sl-textarea.js
@@ -5,11 +5,19 @@ import TooltipEnabled from '../mixins/tooltip-enabled';
  * @module components
  * @class sl-textarea
  */
-export default Ember.TextArea.extend( TooltipEnabled, {
+export default Ember.Component.extend( TooltipEnabled, {
 
     /**
      * Class names for the component
      * @property {array} classNames
      */
-    classNames: [ 'form-control', 'sl-textarea' ]
+    classNames: [ 'form-group', 'sl-textarea' ],
+
+    /**
+     * The ID of the textarea input element
+     * @property {string} inputId
+     */
+    inputId: function () {
+        return this.get( 'elementId' ) + 'Input';
+    }.property( 'elementId' )
 });
