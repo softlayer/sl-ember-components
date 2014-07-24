81781a79737b0dc69f591c21cba7758f505f30fd
Joshua Forisha <jforisha@softlayer.com>
2014-07-24 15:25:00 -0500

    Add local app-only component property-text

---
+++ app/components/property-text.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    classNameBindings: [ 'required:list-group-item-warning' ],
+
+    classNames: [ 'list-group-item' ],
+
+    required: false
+});
