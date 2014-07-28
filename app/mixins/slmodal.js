ca6f792ec637c6ee8bd811406eb2081b42f3d4f2
Jeremy Brown <jebrown@softlayer.com>
2014-07-28 13:36:59 -0500

    Add required attributeBindings values; refactor styles; add comments

--- app/mixins/slmodal.js
+++ app/mixins/slmodal.js
@@ -6,40 +6,117 @@ export default Ember.Mixin.create({
 
     classNames: [ 'modal', 'fade' ],
 
-    /* Empty implementations of modal hooks - override these to implement custom response handling */
+    attributeBindings: [ 'aria-hidden', 'tabindex', 'role' ],
+
+    /**
+     * `aria-hidden` attribute to inform assistive technologies to skip the modal's DOM elements
+     *
+     * @property {string} aria-hidden
+     * @default "true"
+     */
+    'aria-hidden': 'true',
+
+    /**
+     * `tabindex` attribute value
+     *
+     * @property {string} tab index
+     * @default '-1'
+     */
+    tabindex: '-1',
+
+    /**
+     * `role` attribute value
+     *
+     * @property {string} role
+     * @default 'dialog'
+     */
+    role: 'dialog',
+
+    /**
+     * Overridable method stub
+     *
+     * Triggered by Twitter Bootstrap 3 modal's `show.bs.modal` event
+     *
+     * @return {void}
+     */
     showHandler: function(){ },
+
+    /**
+     * Overridable method stub
+     *
+     * Triggered by Twitter Bootstrap 3 modal's `shown.bs.modal` event
+     *
+     * @return {void}
+     */
     shownHandler: function(){ },
+
+    /**
+     * Overridable method stub
+     *
+     * Triggered by Twitter Bootstrap 3 modal's `hide.bs.modal` event
+     *
+     * @return {void}
+     */
     hideHandler: function(){ },
+
+    /**
+     * Overridable method stub
+     *
+     * Triggered by Twitter Bootstrap 3 modal's `hidden.bs.modal` event
+     *
+     * @return {void}
+     */
     hiddenHandler: function(){ },
+
+    /**
+     * Overridable method stub
+     *
+     * Triggered by Twitter Bootstrap 3 modal's `loaded.bs.modal` event
+     *
+     * @return {void}
+     */
     loadedHandler: function(){ },
 
+    /**
+     * On 'didInsertElement', executes any defined handlers for exposed Twitter Bootstrap 3 modal events
+     *
+     * @observes didInsertElement
+     * @return {void}
+     */
     modalize: function() {
-       this.$().modal({
+        this.$().modal({
             keyboard : true,
             show     : false,
             backdrop : 'static'
-        }).on( 'show.bs.modal', function() {
+        })
+
+        .on( 'show.bs.modal', function() {
             this.showHandler();
-        }.bind( this )).on( 'shown.bs.modal', function() {
+        }.bind( this ) )
+
+        .on( 'shown.bs.modal', function() {
             this.shownHandler();
-        }.bind( this )).on( 'hide.bs.modal', function() {
+        }.bind( this ) )
+
+        .on( 'hide.bs.modal', function() {
             this.hideHandler();
-        }.bind( this )).on( 'hidden.bs.modal', function() {
+        }.bind( this ) )
+
+        .on( 'hidden.bs.modal', function() {
             this.hiddenHandler();
-        }.bind( this )).on( 'loaded.bs.modal', function() {
+        }.bind( this ) )
+
+        .on( 'loaded.bs.modal', function() {
             this.loadedHandler();
-        }.bind( this ));
+        }.bind( this ) );
     }.on( 'didInsertElement' ),
 
     actions: {
-
         close: function() {
-
             if ( this.$() ) {
                 this.$().modal( 'hide' );
             }
         }
-
     }
 
 });
