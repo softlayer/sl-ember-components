5bee250d34657aeee8dd4f930f6211538861a935
Joshua Forisha <jforisha@softlayer.com>
2014-07-28 17:24:30 -0500

    Add functionality to sl-radiogroup
    InputBased mixin functionality was added, to enable handling of
    readonly, disabled, optional, and required properties. Radio groups can
    now be disabled as a whole, or by individual radio input.

--- app/components/sl-radiogroup.js
+++ app/components/sl-radiogroup.js
@@ -1,21 +1,22 @@
 import Ember from 'ember';
+import InputBased from '../mixins/input-based';
 import TooltipEnabled from '../mixins/tooltip-enabled';
 
 /**
  * @module components
  * @class sl-radiogroup
  */
-export default Ember.Component.extend( TooltipEnabled, {
+export default Ember.Component.extend( InputBased, TooltipEnabled, {
 
     /**
      * Object of action functions
-     * @property {object} actions
+     * @property {Object} actions
      */
     actions: {
 
         /**
          * Action to take when a radio button has been changed
-         * @method change
+         * @method actions.change
          * @param {mixed} value - The value of the radio input that triggered change
          */
         change: function ( value ) {
@@ -24,25 +25,38 @@ export default Ember.Component.extend( TooltipEnabled, {
     },
 
     /**
+     * Attribute bindings for the component's root element
+     * @property {Array} attributeBindings
+     */
+    attributeBindings: [ 'disabled' ],
+
+    /**
      * Class names for the containing element
-     * @property {array} classNames
+     * @property {Array} classNames
      */
     classNames: [ 'form-group', 'sl-radiogroup' ],
 
     /**
      * Whether the radio buttons should be put inline together
-     * @property {boolean} inline
+     * @property {Boolean} inline
      * @default false
      */
     inline: false,
 
     /**
      * The currently selected radio input
-     * @property {object} selectedInput
+     * @property {Object} selectedInput
      */
     selectedInput: function () {
         return this.$( 'input[value="' + this.get( 'value' ) + '"]' );
     }.property( 'value' ),
+
+    /**
+     * HTML tag name for the component's root element
+     * @property {String} tagName
+     * @default "fieldset"
+     */
+    tagName: 'fieldset',
 
     /**
      * Selects the radio input with the current value
