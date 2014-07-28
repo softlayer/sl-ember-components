8bea829390d9513ccf63ff5daf2f0ec88c7882a0
Joshua Forisha <jforisha@softlayer.com>
2014-07-28 16:24:40 -0500

    Change paths to hash-based for "development" env

--- config/environment.js
+++ config/environment.js
@@ -28,6 +28,8 @@ module.exports = function(environment) {
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
+
+    ENV.locationType = 'hash';
   }
 
   if (environment === 'production') {
