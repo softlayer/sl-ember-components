7f19897aa3ba4e7323b1a761d38572d2fd8e8a19
Joshua Forisha <jforisha@softlayer.com>
2014-08-13 09:20:48 -0500

    Improvement for popover; set placement to top

--- app/mixins/sl-tooltip-enabled.js
+++ app/mixins/sl-tooltip-enabled.js
@@ -6,25 +6,25 @@ export default Ember.Mixin.create({
      * Attribute bindings for mixin's component element
      * @property {array} attributeBindings
      */
-    attributeBindings: [ 'popover', 'title' ],
+    attributeBindings: [ 'title' ],
 
     /**
      * Enables the tooltip functionality, based on a passed-in `title` attribute
      * @method enableTooltip
      */
     enableTooltip: function () {
-        if ( this.get( 'title' )) {
-            var popoverContent = this.get( 'popover' );
+        var popoverContent = this.get( 'popover' );
+        var title = this.get( 'title' );
 
-            if ( popoverContent ) {
-                this.set( 'data-toggle', 'popover' );
-                this.$().popover({
-                    content: popoverContent
-                });
-            } else {
-                this.set( 'data-toggle', 'tooltip' );
-                this.$().tooltip();
-            }
+        if ( popoverContent ) {
+            this.set( 'data-toggle', 'popover' );
+            this.$().popover({
+                content: popoverContent,
+                placement: 'top'
+            });
+        } else if ( title ) {
+            this.set( 'data-toggle', 'tooltip' );
+            this.$().tooltip();
         }
     }.on( 'didInsertElement' )
 });
