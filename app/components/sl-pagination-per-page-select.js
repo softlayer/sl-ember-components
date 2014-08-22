bfedf9afb9ad46db57a5def867aa447e5c99397c
Matt Marcum <mmarcum@softlayer.com>
2014-08-22 15:51:38 -0500

    swapped out ember.select for sl-select on pagination-per-page

--- app/components/sl-pagination-per-page-select.js
+++ app/components/sl-pagination-per-page-select.js
@@ -2,9 +2,5 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     tagName: 'span',
-    classNames: [ 'sl-pagination-per-page-select form-inline' ],
-    perPage: Ember.computed.oneWay( 'itemCountPerPage' ),
-    perPageObserver: function(){
-        this.sendAction( 'action', this.get( 'perPage' ) );
-    }.observes( 'perPage' )
+    classNames: [ 'sl-pagination-per-page-select form-inline' ]
 });
