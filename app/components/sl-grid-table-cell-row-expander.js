377e451f6a5fc89317fdb41c621618ad018c60cf
Matt Marcum <mmarcum@softlayer.com>
2014-08-25 10:18:58 -0500

    row expaded td has click action now

--- app/components/sl-grid-table-cell-row-expander.js
+++ app/components/sl-grid-table-cell-row-expander.js
@@ -3,9 +3,12 @@ import SlGridTableCell from './sl-grid-table-cell';
 
 export default SlGridTableCell.extend({
     actions: {
-        expandRow: function(){
-            this.toggleProperty( 'row.rowExpanderIsOpen' );
+        toggleRowExpander: function(){
+            //dummy action, click will fire below
         }
+    },
+    click: function(){
+        this.toggleProperty( 'row.rowExpanderIsOpen' );
     },
     classNames: [ 'sl-grid-table-cell-expander' ],
     expanded: Ember.computed.bool( 'row.rowExpanderIsOpen' )
