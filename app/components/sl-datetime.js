19b0f43518d0df1ba11b1f36d01bf9b655dc25bb
Joshua Forisha <jforisha@softlayer.com>
2014-08-28 12:58:38 -0500

    Adjust datetime value binding, add documentation

--- app/components/sl-datetime.js
+++ app/components/sl-datetime.js
@@ -1,24 +1,43 @@
 import Ember from 'ember';
 import TooltipEnabled from '../mixins/sl-tooltip-enabled';
 
+/**
+ * @module components
+ * @class sl-datetime
+ */
 export default Ember.Component.extend( TooltipEnabled, {
-    attributeBindings: [ 'datetime' ],
-
-    classNames: [ 'sl-datetime' ],
-
-    datetime: function () {
-        return moment( this.get( 'value' ));
-    }.property( 'value' ),
 
     /**
-     * "date", "datetime", "relative"
+     * Bindings for the datetime's attribute values
+     * @property {array} attributeBindings
      */
-    format: 'datetime',
+    attributeBindings: [ 'datetime' ],
 
-    fullDatetime: function () {
+    /**
+     * Class names for the component's root element, <time>
+     * @property {array} classNames
+     */
+    classNames: [ 'sl-datetime' ],
+
+    /**
+     * The datetime's value formatted as a datetime string
+     * @property {string} datetime
+     */
+    datetime: function () {
         return moment( this.get( 'value' )).format( 'YYYY-MM-DD HH:mm A [GMT]ZZ' );
     }.property( 'format', 'value' ),
 
+    /**
+     * String name for the format to render inline. Can be "date", "datetime",
+     * or "relative".
+     * @property {string} format
+     */
+    format: 'datetime',
+
+    /**
+     * Formatted string based on value and supplied format
+     * @property {string} formattedValue
+     */
     formattedValue: function () {
         var momentValue = moment( this.get( 'value' ));
 
@@ -29,12 +48,26 @@ export default Ember.Component.extend( TooltipEnabled, {
         }
     }.property( 'format', 'value' ),
 
+    /**
+     * The HTML tag type of the component's root element
+     * @property {string} tagName
+     * @default "time"
+     */
     tagName: 'time',
 
+    /**
+     * The text to use for the component's tooltip
+     * @property {string} title
+     */
     title: function () {
-        return this.get( 'fullDatetime' );
-    }.property( 'fullDatetime' ),
+        return this.get( 'datetime' );
+    }.property( 'datetime' ),
 
+    /**
+     * The bound value of the component's date value
+     * @property {date|string} value
+     * @default new Date()
+     */
     value: function () {
         return new Date();
     }.property()
