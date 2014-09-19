6bd9578838bd0a87d126729090db9054913667da
Joshua Forisha <jforisha@softlayer.com>
2014-09-19 16:53:36 -0500

    Re-apply ENV settings

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -21,10 +21,12 @@ module.exports = function(environment) {
 
   if (environment === 'development') {
     // ENV.APP.LOG_RESOLVER = true;
-    ENV.APP.LOG_ACTIVE_GENERATION = true;
+    // ENV.APP.LOG_ACTIVE_GENERATION = true;
     // ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    ENV.APP.LOG_VIEW_LOOKUPS = true;
+    // ENV.APP.LOG_VIEW_LOOKUPS = true;
+
+    ENV.locationType = 'hash';
   }
 
   if (environment === 'test') {
