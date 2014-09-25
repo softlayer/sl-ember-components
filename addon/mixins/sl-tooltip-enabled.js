c7272eed8e80006680a5348c29a25619445526c9
Joshua Forisha <jforisha@softlayer.com>
2014-09-25 09:49:12 -0500

    Improve style of tooltip demo page, simplify tooltip

--- addon/mixins/sl-tooltip-enabled.js
+++ addon/mixins/sl-tooltip-enabled.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
 
+/**
+ * @module mixins
+ * @class sl-tooltip-enabled
+ */
 export default Ember.Mixin.create({
 
     /**
      * Enables the tooltip functionality, based on component's `title` attribute
+     *
      * @method enableTooltip
      */
     enableTooltip: function () {
@@ -14,44 +19,18 @@ export default Ember.Mixin.create({
             return;
         }
 
-        if ( this.get( 'isPopover' )) {
-            this.$().popover( 'destroy' );
-            this.set( 'isPopover', false );
-        }
-
-        if ( this.get( 'isTooltip' )) {
-            this.$().tooltip( 'destroy' );
-            this.set( 'isTooltip', false );
-        }
-
         if ( popoverContent ) {
             this.set( 'data-toggle', 'popover' );
             this.$().popover({
                 content: popoverContent,
                 placement: 'top'
             });
-            this.set( 'isPopover', false );
         } else if ( title ) {
             this.set( 'data-toggle', 'tooltip' );
             this.$().tooltip({
                 container: 'body',
                 title: title
             });
-            this.set( 'isTooltip', true );
         }
-    }.observes( 'popover', 'title' ).on( 'didInsertElement' ),
-
-    /**
-     * Whether the component has been set up as a popover
-     * @property {boolean} isPopover
-     * @default false
-     */
-    isPopover: false,
-
-    /**
-     * Whether the component has been set up as a tooltip
-     * @property {boolean} isTooltip
-     * @default false
-     */
-    isTooltip: false
+    }.observes( 'popover', 'title' ).on( 'didInsertElement' )
 });
