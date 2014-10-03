116d1ac1f05acd1ae277f541437a32026a299e61
Joshua Forisha <jforisha@softlayer.com>
2014-10-03 10:00:38 -0500

    Add POC for dynamic sizing of sl-split-grid

--- tests/dummy/app/controllers/split-grid.js
+++ tests/dummy/app/controllers/split-grid.js
@@ -31,5 +31,26 @@ export default Ember.ArrayController.extend({
             action: 'logRecord',
             label: 'Log'
         }
-    ]
+    ],
+
+    setupUpdateGridHeight: function() {
+        Ember.$( window ).on( 'resize', this.updateGridHeight );
+    }.on( 'init' ),
+
+    updateGridHeight: function() {
+        // TEMP: This is just a proof of concept for dynamically sizing the
+        // sl-split-grid-body's height dependending on viewport and other
+        // elements. This logic will eventually be optionally built into the
+        // component itself.
+
+        setTimeout( function() {
+            var viewportHeight = Ember.$( window ).innerHeight(),
+                topPosition = Ember.$( '.sl-split-grid' ).position().top,
+                gridHeaderHeight = Ember.$( '.sl-split-grid-header' ).height(),
+                gridHeadHeight = Ember.$( '.sl-split-grid-head' ).height(),
+                newGridBodyHeight = Math.max( 0, viewportHeight - topPosition - gridHeaderHeight - gridHeadHeight - 2 );
+
+            Ember.$( '.sl-split-grid-body' ).height( newGridBodyHeight );
+        }, 200 );
+    }.on( 'init' )
 });
