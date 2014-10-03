12832f077bb975f7905a12768bf790fa2ecf33c7
Joshua Forisha <jforisha@softlayer.com>
2014-10-03 15:01:38 -0500

    Remove placeholder tests, update sl-menu test

--- tests/unit/components/sl-menu-test.js
+++ tests/unit/components/sl-menu-test.js
@@ -3,174 +3,153 @@ import { test, moduleFor, moduleForComponent } from 'ember-qunit';
 import SlMenu from 'sl-components/components/sl-menu';
 
 var clickCounter = 0,
-    modelStub = Ember.Object.create({
-            label: null,
-            pages: Ember.A([
-                Ember.Object.create({ label: 'Top Level A', pages: Ember.A([
-                    Ember.Object.create({ label: 'Option A1', action: 'MyAction' }),
-                    Ember.Object.create({ label: 'Option A2', action: function(){ clickCounter = 0; clickCounter++; }}),
-                    Ember.Object.create({ label: 'Option A3', action: { actionName: 'MyAction', data: { name: 'Joe' }}}),
-                    Ember.Object.create({ label: 'Sub Menu A3', pages: Ember.A([
-                        Ember.Object.create({ label: 'Sub Sub Menu A3.1' }),
-                        Ember.Object.create({ label: 'Sub Sub Menu A3.2' }),
-                        Ember.Object.create({ label: 'Sub Sub Menu A3.3' }),
-                        Ember.Object.create({ label: 'Sub Sub Menu A3.4' })
-                    ])})
-                ])}),
-                Ember.Object.create({ label: 'Top Level B' }),
-                Ember.Object.create({ label: 'Top Level C' })
-            ])
-        });
+    modelStub = {
+        label: null,
+        pages: [
+            { label: 'Top Level A', pages: [
+                { label: 'Option A1', action: 'MyAction' },
+                { label: 'Option A2', action: function(){ clickCounter = 0; clickCounter++; }},
+                { label: 'Option A3', action: { actionName: 'MyAction', data: { name: 'Joe' }}},
+                { label: 'Sub Menu A3', pages: [
+                    { label: 'Sub Sub Menu A3.1' },
+                    { label: 'Sub Sub Menu A3.2' },
+                    { label: 'Sub Sub Menu A3.3' },
+                    { label: 'Sub Sub Menu A3.4' }
+                ]}
+            ]},
+            { label: 'Top Level B' },
+            { label: 'Top Level C' }
+        ]
+    };
 
 moduleForComponent( 'sl-menu', 'Component: Sl-Menu' );
 
 test( 'Menu creates correct DOM structure', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
-    var $component = this.append();
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append();
 
     expect( 1 );
-    equal( $component.find( 'li' ).length, 12 );
+
+    equal( $component.find( 'li' ).length, 11 );
 });
 
 test( 'Menu properly hides all but root list', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
-    var $component = this.append();
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append();
 
     expect( 1 );
-    equal( $component.find( 'li:visible' ).length, 4 );
+
+    equal( $component.find( 'li:visible' ).length, 3 );
 });
 
 test( 'Menu shows child on hover', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
-    var $component = this.append();
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append();
 
     expect( 2 );
-    equal( $component.find( 'li:visible' ).length, 4 );
+
+    equal( $component.find( 'li:visible' ).length, 3 );
 
     $component.find( 'li:visible' ).first().mouseenter();
-    equal( $component.find( 'li:visible' ).length, 8 );
+    equal( $component.find( 'li:visible' ).length, 7 );
 });
 
 test( 'Menu closes child on mouse exit', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
-    var $component = this.append();
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append(),
+        child = $component.find( 'li:visible' ).first();
 
     expect( 3 );
-    equal( $component.find( 'li:visible' ).length, 4 );
 
-    var child = $component.find( 'li:visible' ).first();
+    equal( $component.find( 'li:visible' ).length, 3 );
 
     child.mouseenter();
-    equal( $component.find( 'li:visible' ).length, 8 );
+    equal( $component.find( 'li:visible' ).length, 7 );
 
     child.mouseleave();
-    equal( $component.find( 'li:visible' ).length, 4 );
+    equal( $component.find( 'li:visible' ).length, 3 );
 });
 
 test( 'Menu click supports native function', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
-
-    var $component = this.append();
-
-    var child = $component.find( 'li:visible' ).first();
-    child.mouseenter();
-    var targetMenuItem = child.find( 'li:visible' )[1];
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append(),
+        child = $component.find( 'li:visible' ).first();
 
     expect( 2 );
 
     equal( clickCounter, 0 );
-    targetMenuItem.click();
+
+    child.mouseenter();
+    child.find( 'li:visible' )[ 1 ].click();
     equal( clickCounter, 1 );
 });
 
 test( 'Menu click supports action names', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
-
-    var $component = this.append();
-
-    var child = $component.find( 'li:visible' ).first();
-    child.mouseenter();
-    var targetMenuItem = child.find( 'li:visible' )[0];
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append(),
+        child = $component.find( 'li:visible' ).first(),
+        targetObject = {
+            actionHandler: function( actionName ) {
+                equal( actionName, 'MyAction' );
+            }
+        };
 
     expect( 1 );
-    var targetObject = {
-        actionHandler: function( actionName ) {
-            equal( actionName, 'MyAction' );
-        }
-    };
 
+    child.mouseenter();
     component.set( 'actionInitiated', 'actionHandler' );
     component.set( 'targetObject', targetObject );
-
-    targetMenuItem.click();
+    child.find( 'li:visible' )[ 0 ].click();
 });
 
 test( 'Menu click supports action names with supporting data', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
-
-    var $component = this.append();
-
-    var child = $component.find( 'li:visible' ).first();
-    child.mouseenter();
-    var targetMenuItem = child.find( 'li:visible' )[2];
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append(),
+        child = $component.find( 'li:visible' ).first(),
+        targetObject = {
+            actionHandler: function( actionName, data ) {
+                equal( actionName, 'MyAction' );
+                equal( data.name, 'Joe' );
+            }
+        };
 
     expect( 2 );
-    var targetObject = {
-        actionHandler: function( actionName, data ) {
-            equal( actionName, 'MyAction' );
-            equal( data.name, 'Joe' );
-        }
-    };
 
+    child.mouseenter();
     component.set( 'actionInitiated', 'actionHandler' );
     component.set( 'targetObject', targetObject );
-
-    targetMenuItem.click();
+    child.find( 'li:visible' )[ 2 ].click();
 });
 
 test( 'Menu selection fires proper selection event', function() {
-    var component = this.subject({
-        menu: modelStub
-    });
+    var component = this.subject({ menu: modelStub }),
+        $component = this.append(),
+        selectionCounter = 0,
+        targetObject = {
+            selectionHandler: function( path ) {
+                selectionCounter++;
+            }
+        },
+        child1,
+        child2,
+        child3;
 
-    var $component = this.append();
-
-    var selectionCounter = 0;
-    var targetObject = {
-        selectionHandler: function( path ) {
-            selectionCounter++;
-        }
-    };
+    expect( 1 );
 
     component.set( 'selectionMade', 'selectionHandler' );
     component.set( 'targetObject', targetObject );
 
-    var child = $component.find( 'li:visible' ).first();
-    var $child = child;
-    $child.mouseenter();
-    $child.click();
-    var child2 = child.find( 'li:visible' )[3];
-    var $child2 = $( child2 );
-    $child2.mouseenter();
-    $child2.click();
-    var child3 = $child2.find( 'li:visible' )[0];
-    var $child3 = $( child3 );
-    $child3.mouseenter();
-    $child3.click();
+    child1 = $component.find( 'li:visible' ).first();
+    child1.trigger( 'mouseenter' );
+    child1.trigger( 'click' );
+
+    child2 = child1.find( 'li:visible' ).last();
+    child2.trigger( 'mouseenter' );
+    child2.trigger( 'click' );
+
+    child3 = child2.find( 'li:visible' ).first();
+    child3.trigger( 'mouseenter' );
+    child3.trigger( 'click' );
 
     equal( selectionCounter, 3 );
 });
