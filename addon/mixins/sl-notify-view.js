41fe2225a6307deea0bb583d672bfff999ff563d
Jeremy Brown <jebrown@softlayer.com>
2014-10-15 08:49:25 -0500

    Add additional notifications

--- addon/mixins/sl-notify-view.js
+++ addon/mixins/sl-notify-view.js
@@ -1,27 +1,45 @@
 import Ember from 'ember';
 
-/**
- * @module mixins
- * @class sl-notify-view
- */
+/** @module sl-components/mixins/sl-notify-view */
 export default Ember.Mixin.create({
 
     /**
      * Notify the view's controller by sending "viewDidInsertElement"
      *
-     * @method notifyDidInsertElement
+     * @function notifyDidInsertElement
+     * @return   {void}
      */
     notifyDidInsertElement: function() {
         this.get( 'controller' ).send( 'viewDidInsertElement' );
     }.on( 'didInsertElement' ),
 
     /**
+     * Notify the view's controller by sending "viewWillClearRender"
+     *
+     * @function notifyWillClearRender
+     * @return   {void}
+     */
+    notifyWillClearRender: function() {
+        this.get( 'controller' ).send( 'viewWillClearRender' );
+    }.on( 'willClearRender' ),
+
+    /**
      * Notify the view's controller by sending "viewWillDestroyElement"
      *
-     * @method notifyWillDestroyElement
+     * @function notifyWillDestroyElement
+     * @return   {void}
      */
     notifyWillDestroyElement: function() {
         this.get( 'controller' ).send( 'viewWillDestroyElement' );
-    }.on( 'willDestroyElement' )
+    }.on( 'willDestroyElement' ),
 
+    /**
+     * Notify the view's controller by sending "viewWillInsertElement"
+     *
+     * @function notifyWillInsertElement
+     * @return   {void}
+     */
+    notifyWillInsertElement: function() {
+        this.get( 'controller' ).send( 'viewWillInsertElement' );
+    }.on( 'willInsertElement' ),
 });
