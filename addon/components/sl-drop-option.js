5a9863aedb9d998e28bd1870af4ed7962068d27a
Jeremy Brown <jebrown@softlayer.com>
2014-10-21 14:49:19 -0500

    Refactor to follow style guides

--- addon/components/sl-drop-option.js
+++ addon/components/sl-drop-option.js
@@ -1,22 +1,46 @@
 import Ember from 'ember';
 
-/**
- * @module components
- * @class sl-drop-option
- */
+/** @module sl-components/components/sl-drop-option */
 export default Ember.Component.extend({
+
+    /**
+     * HTML tag name for the root element
+     *
+     * @property {string}       tagName
+     * @type     {Ember.String}
+     * @default  "li"
+     */
+    tagName: 'li',
+
+    /**
+     * Class names for the root element
+     *
+     * @property {array}       classNames
+     * @type     {Ember.Array}
+     */
+    classNames: [ 'sl-drop-option' ],
+
+    /**
+     * Class name bindings for the root element
+     *
+     * @property {array}       classNameBindings
+     * @type     {Ember.Array}
+     */
+    classNameBindings: [ 'optionType' ],
 
     /**
      * Component actions hash
      *
-     * @property {object} actions
+     * @property {object}       actions
+     * @type     {Ember.Object}
      */
     actions: {
 
         /**
          * Send the primary action when the click action is triggered
          *
-         * @method actions.click
+         * @function actions.click
+         * @return   {void}
          */
         click: function() {
             this.sendAction( 'action' );
@@ -24,34 +48,15 @@ export default Ember.Component.extend({
     },
 
     /**
-     * Class name bindings for the root element
-     *
-     * @property {array} classNameBindings
-     */
-    classNameBindings: [ 'optionType' ],
-
-    /**
-     * Class names for the root element
-     *
-     * @property {array} classNames
-     */
-    classNames: [ 'sl-drop-option' ],
-
-    /**
      * Represents the type of option; "divider" if the label is undefined, or
      * "presentation" otherwise
      *
-     * @property {string} optionType
+     * @function optionType
+     * @type     {Ember.String}
+     * @observes 'label'
+     * @return   {string}
      */
     optionType: function() {
         return this.get( 'label' ) ? 'presentation' : 'divider';
-    }.property( 'label' ),
-
-    /**
-     * HTML tag name for the root element
-     *
-     * @property {string} tagName
-     * @default "li"
-     */
-    tagName: 'li'
+    }.property( 'label' )
 });
