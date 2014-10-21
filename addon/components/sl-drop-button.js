f465d99325b3e7a258d185e3b7bcfe85fd5ea72f
Jeremy Brown <jebrown@softlayer.com>
2014-10-21 11:23:38 -0500

    Refactor to follow style guides

--- addon/components/sl-drop-button.js
+++ addon/components/sl-drop-button.js
@@ -1,23 +1,39 @@
 import Ember from 'ember';
 import TooltipEnabled from '../mixins/sl-tooltip-enabled';
 
-/**
- * @module components
- * @class sl-drop-button
- */
+/** @module sl-components/components/sl-drop-button */
 export default Ember.Component.extend( TooltipEnabled, {
+
+    /**
+     * Class names for the div element
+     *
+     * @property {array}       classNames
+     * @type     {Ember.Array}
+     */
+    classNames: [ 'btn-group', 'dropdown', 'sl-drop-button' ],
+
+    /**
+     * Class attribute bindings for the button
+     *
+     * @property {array}       classNameBindings
+     * @type     {Ember.Array}
+     */
+    classNameBindings: [ 'themeClass' ],
 
     /**
      * Component actions hash
      *
-     * @property {object} actions
+     * @property {object}       actions
+     * @type     {Ember.Object}
      */
     actions: {
 
         /**
          * Used to trigger specific option-bound action
          *
-         * @method click
+         * @function click
+         * @argument {string}  action to trigger
+         * @return   {void}
          */
         click: function( action ) {
             this.triggerAction({ action: action });
@@ -25,24 +41,11 @@ export default Ember.Component.extend( TooltipEnabled, {
     },
 
     /**
-     * Class attribute bindings for the button
-     *
-     * @property {array} classNameBindings
-     */
-    classNameBindings: [ 'themeClass' ],
-
-    /**
-     * Class names for the div element
-     *
-     * @property {array} classNames
-     */
-    classNames: [ 'btn-group', 'dropdown', 'sl-drop-button' ],
-
-    /**
      * Class string for the button's icon
      *
      * @property {string} iconClass
-     * @default "caret"
+     * @type     {Ember.String}
+     * @default  "caret"
      */
     iconClass: 'caret',
 
@@ -50,14 +53,17 @@ export default Ember.Component.extend( TooltipEnabled, {
      * The string name of the style theme for the button
      *
      * @property {string} theme
-     * @default "default"
+     * @type     {Ember.String}
+     * @default  "default"
      */
     theme: 'default',
 
     /**
      * The class value for the drop-button based on the current "theme"
      *
-     * @property {string} themeClass
+     * @function themeClass
+     * @observes 'theme'
+     * @return   {string}
      */
     themeClass: function() {
         return 'dropdown-' + this.get( 'theme' );
