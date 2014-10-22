d0406e997cebd7953658c4cb29f3d602fa5b4da1
Jeremy Brown <jebrown@softlayer.com>
2014-10-22 15:05:46 -0500

    Refactor to follow style guides

--- addon/components/sl-pagination-controls.js
+++ addon/components/sl-pagination-controls.js
@@ -1,35 +1,41 @@
 import Ember from 'ember';
 
-/**
- * @module components
- * @class sl-pagination-controls
- */
+/** @module sl-components/components/sl-pagination-controls */
 export default Ember.Component.extend({
+
+    /**
+     * HTML tag name for the root element
+     *
+     * @property {Ember.String} tagName
+     * @default  "span"
+     */
+    tagName: 'span',
+
+    /**
+     * Class names for root element
+     *
+     * @property {Ember.Array} classNames
+     */
+    classNames: [ 'sl-pagination-controls' ],
 
     /**
      * Component actions hash
      *
-     * @property {object} actions
+     * @property {Ember.Object} actions
      */
     actions: {
 
         /**
          * Action triggered for changing pages
          *
-         * @method changePage
-         * @param {number} page - The page number being changed to
+         * @function actions.changePage
+         * @argument {number} page - The page number being changed to
+         * @return {void}
          */
         changePage: function( page ) {
-            this.sendAction( 'action', page ? page : this.get( 'currentPageInput' ));
+            this.sendAction( 'action', page ? page : this.get( 'currentPageInput' ) );
         }
     },
-
-    /**
-     * Class names for root element
-     *
-     * @property {array} classNames
-     */
-    classNames: [ 'sl-pagination-controls' ],
 
     /**
      * Read-only binding to current page number
@@ -42,51 +48,45 @@ export default Ember.Component.extend({
      * Whether the page input is disabled
      *
      * @property {boolean} currentPageInputDisabled
-     * @default false
+     * @default  false
      */
     currentPageInputDisabled: Ember.computed.alias( 'disabled' ),
 
     /**
+     * When true, the last link control is disabled
+     *
+     * @property {boolean} lastLinkDisabled
+     * @default  false;
+     */
+    lastLinkDisabled: Ember.computed.alias( 'nextLinkDisabled' ),
+
+    /**
+     * When true, the previous link control is disabled
+     *
+     * @property {boolean} prevLinkDisabled
+     * @default  false
+     */
+    prevLinkDisabled: Ember.computed.alias( 'firstLinkDisabled' ),
+
+    /**
      * When true, the first link control is disabled
      *
-     * @property {boolean} firstLinkDisabled
-     * @default false
+     * @function firstLinkDisabled
+     * @observes currentPage, disabled
+     * @return   {boolean}
      */
     firstLinkDisabled: function() {
         return this.get( 'disabled' ) || this.get( 'currentPage' ) === 1;
     }.property( 'currentPage', 'disabled' ),
 
     /**
-     * When true, the last link control is disabled
-     *
-     * @property {boolean} lastLinkDisabled
-     * @default false;
-     */
-    lastLinkDisabled: Ember.computed.alias( 'nextLinkDisabled' ),
-
-    /**
      * When true, the next link control is disabled
      *
-     * @property {boolean} nextLinkDisabled
-     * @default false
+     * @function nextLinkDisabled
+     * @observes currentPage, disabled, totalPages
+     * @return   {boolean}
      */
     nextLinkDisabled: function() {
         return this.get( 'disabled' ) || this.get( 'currentPage' ) === this.get( 'totalPages' );
-    }.property( 'currentPage', 'disabled', 'totalPages' ),
-
-    /**
-     * When true, the previous link control is disabled
-     *
-     * @property {boolean} prevLinkDisabled
-     * @default false
-     */
-    prevLinkDisabled: Ember.computed.alias( 'firstLinkDisabled' ),
-
-    /**
-     * HTML tag name for the root element
-     *
-     * @property {string} tagName
-     * @default "span"
-     */
-    tagName: 'span'
+    }.property( 'currentPage', 'disabled', 'totalPages' )
 });
