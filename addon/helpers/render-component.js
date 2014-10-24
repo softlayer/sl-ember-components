ba2bb9cb4ed88875a5a8e7de055331a58568bd0d
Joshua Forisha <jforisha@softlayer.com>
2014-10-24 10:08:10 -0500

    Add jsdoc block info to helpers

--- addon/helpers/render-component.js
+++ addon/helpers/render-component.js
@@ -7,11 +7,16 @@ import Ember from 'ember';
  *
  * @example
  * {{render-component 'sl-grid-table-cell-link' foo=bar doo=car }}
+ *
+ * @module   helpers
+ * @function render-component
+ * @param    {string} componentPath - Lookup path for the component name
+ * @returns  {string} The rendered component
  */
 export default function( componentPath ) {
-    var options = arguments[ arguments.length - 1 ],
+    var options   = arguments[ arguments.length - 1 ],
         component = Ember.Handlebars.get( this, componentPath, options ),
-        helper = Ember.Handlebars.resolveHelper( options.data.view.container, component );
+        helper    = Ember.Handlebars.resolveHelper( options.data.view.container, component );
 
     helper.call( this, options );
 }
