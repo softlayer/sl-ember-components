33604c3237402f315d245d81fed18af6431b752c
Jeremy Brown <jebrown@softlayer.com>
2014-10-28 11:57:19 -0500

    Adjust formatting to follow style guide

--- addon/mixins/sl-tooltip-enabled.js
+++ addon/mixins/sl-tooltip-enabled.js
@@ -35,7 +35,7 @@ export default Ember.Mixin.create({
      * Enable the tooltip functionality, based on component's `title` attribute
      *
      * @function enableTooltip
-     * @observes popover, title, "didInsertElement" event
+     * @observes "didInsertElement" event, popover, title
      * @returns  {void}
      */
     enableTooltip: function() {
@@ -49,14 +49,14 @@ export default Ember.Mixin.create({
         if ( popoverContent ) {
             this.set( 'data-toggle', 'popover' );
             this.$().popover({
-                content: popoverContent,
-                placement: 'top'
+                content   : popoverContent,
+                placement : 'top'
             });
         } else {
             this.set( 'data-toggle', 'tooltip' );
             this.$().tooltip({
-                container: 'body',
-                title: title
+                container : 'body',
+                title     : title
             });
         }
     }.observes( 'popover', 'title' ).on( 'didInsertElement' )
