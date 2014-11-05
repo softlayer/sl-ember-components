778ed61cc36b588a82008c520cfc089f247fc6fd
Jeremy Brown <jebrown@softlayer.com>
2014-11-05 14:23:08 -0600

    Code requires use of Ember.Object.create() call

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     setupController: function() {
-        this.controllerFor( 'pagination' ).get( 'translateService' ).setDictionary({
+        this.controllerFor( 'pagination' ).get( 'translateService' ).setDictionary( Ember.Object.create({
            'PAGINATION_DISPLAYING'           : 'Displaying',
            'DEVICE_LIST_PAGINATION_LABEL'    : 'Viewing {0} to {1} of {2} Devices',
            'DEVICE_LIST_PAGINATION_PER_PAGE' : ' per page'
-        });
+        }));
     }
 });
