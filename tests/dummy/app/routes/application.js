7b10f2dafdce6ae40ccc5c83f0c6da8e516ccc7c
Jeremy Brown <jebrown@softlayer.com>
2014-11-05 10:00:45 -0600

    Refactor to resolve odd ArrayProxy errors on sl-tab-panel route

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,17 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model: function() {
-        return Ember.Object.create({
-           'PAGINATION_DISPLAYING': 'Displaying',
-           'DEVICE_LIST_PAGINATION_LABEL': 'Viewing {0} to {1} of {2} Devices',
-           'DEVICE_LIST_PAGINATION_PER_PAGE': ' per page'
+    setupController: function() {
+        this.controllerFor( 'pagination' ).get( 'translateService' ).setDictionary({
+           'PAGINATION_DISPLAYING'           : 'Displaying',
+           'DEVICE_LIST_PAGINATION_LABEL'    : 'Viewing {0} to {1} of {2} Devices',
+           'DEVICE_LIST_PAGINATION_PER_PAGE' : ' per page'
         });
-    },
-
-    setupController: function( controller, model ) {
-        this._super.apply( this, arguments );
-
-        this.controllerFor( 'pagination' ).get( 'translateService' ).setDictionary( model );
     }
 });
