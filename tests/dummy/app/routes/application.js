7019cf94cc3bcc2914f7b457cf6e67edec3baba4
Jeremy Brown <jebrown@softlayer.com>
2014-11-05 09:05:06 -0600

    Refactor for better example of usage

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,13 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    beforeModel: function() {
-        var translations = Ember.Object.create({
+    model: function() {
+        return Ember.Object.create({
            'PAGINATION_DISPLAYING': 'Displaying',
            'DEVICE_LIST_PAGINATION_LABEL': 'Viewing {0} to {1} of {2} Devices',
            'DEVICE_LIST_PAGINATION_PER_PAGE': ' per page'
         });
+    },
 
-        this.controllerFor( 'pagination' ).get( 'translateService' ).setDictionary( translations );
+    setupController: function( controller, model ) {
+        this._super.apply( this, arguments );
+
+        this.controllerFor( 'pagination' ).get( 'translateService' ).setDictionary( model );
     }
 });
