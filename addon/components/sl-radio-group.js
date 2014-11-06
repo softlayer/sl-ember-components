7490a5a910ee8b6a1129cd82773f66a83e9f2bc3
Jeremy Brown <jebrown@softlayer.com>
2014-11-06 11:57:56 -0600

    Refactor to remove unnecessary circular logic surrounding the selection of a sl-radio component based on provided value

--- addon/components/sl-radio-group.js
+++ addon/components/sl-radio-group.js
@@ -73,12 +73,19 @@ export default Ember.Component.extend( InputBased, TooltipEnabled, {
      */
     initialize: function() {
         var name       = this.get( 'name' ),
+            value      = this.get( 'value' ),
             isDisabled = this.get( 'disabled' ),
             isInline   = this.get( 'inline' ),
             isReadonly = this.get( 'readonly' );
 
         Ember.assert( 'The name property must be set on the sl-radio-group component', name )
 
+        /**
+         * To each sl-radio component apply...
+         *
+         * - Attributes: name, disabled, readonly
+         * - Classes: radio, radio-inline
+         */
         this.$( '.sl-radio' ).each( function () {
             var radio = Ember.$( this ),
                 input = Ember.$( 'input', radio );
@@ -106,34 +113,15 @@ export default Ember.Component.extend( InputBased, TooltipEnabled, {
             }
         });
 
+        // Pre-select radio button if a value is set
+        if ( value ) {
+            this.$('input[name=' + name + ']:radio[value=' + value + ']').prop( 'checked', true );
+        }
+
+        // Apply change() listener to keep group value in sync with select sl-radio option
         this.$('input[name=' + name + ']:radio').change( function () {
             this.set( 'value', this.$('input[name=' + name + ']:radio').filter(':checked').val() );
         }.bind(this));
-    }.on( 'didInsertElement' ),
 
-    /**
-     * Selects the radio input with the current value
-     *
-     * @function updateSelection
-     * @observes "didInsertElement" event, value
-     * @returns  {void}
-     */
-    updateSelection: function() {
-        this.get( 'selectedInput' ).prop( 'checked', true );
-    }.observes( 'value' ).on( 'didInsertElement' ),
-
-    // -------------------------------------------------------------------------
-    // Methods
-
-    /**
-     * The currently selected radio input
-     *
-     * @property selectedInput
-     * @observes value
-     * @returns  {object}
-     */
-    selectedInput: function() {
-        return this.$( 'input[value="' + this.get( 'value' ) + '"]' );
-    }.property( 'value' )
-
+    }.on( 'didInsertElement' )
 });
