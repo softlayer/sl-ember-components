3bb08efb2204fd5ecba3ce8bdd9e92531874043d
Jeremy Brown <jebrown@softlayer.com>
2014-12-29 08:45:06 -0600

    Closes softlayer/sl-ember-components#142

--- Brocfile.js
+++ Brocfile.js
@@ -1,9 +1,11 @@
 /* jshint node: true */
 /* global require, module, process */
 
-var EmberAddon = require('ember-cli/lib/broccoli/ember-addon'),
+var EmberAddon   = require( 'ember-cli/lib/broccoli/ember-addon' ),
+    replace      = require( 'broccoli-string-replace' ),
     isProduction = ( process.env.EMBER_ENV || 'development' ) === 'production',
-    app = new EmberAddon();
+    app          = new EmberAddon(),
+    tree;
 
 // Use `app.import` to add additional libraries to the generated
 // output files.
@@ -24,4 +26,18 @@ if ( !isProduction ) {
     app.import( app.bowerDirectory + '/sinon-qunit/lib/sinon-qunit.js', { type: 'test' } );
 }
 
-module.exports = app.toTree();
+tree = replace( app.toTree(), {
+    files: [ 'index.html' ],
+    patterns: [
+        {
+            match: /REPLACE_META_DESCRIPTION/g,
+            replacement: require('./package.json')['description']
+        },
+        {
+            match: /REPLACE_META_KEYWORDS/g,
+            replacement: require('./package.json')['keywords'].join( ', ' ) + ', ember, ember cli'
+        }
+    ]
+});
+
+module.exports = tree;
