175c6b925cacc2ace959836a64fef3481d311ccc
Joshua Forisha <jforisha@softlayer.com>
2015-03-11 09:21:17 -0500

    Improve sl-radio and sl-radio-group tests

--- tests/unit/components/sl-radio-group-test.js
+++ tests/unit/components/sl-radio-group-test.js
@@ -6,42 +6,48 @@ moduleForComponent( 'sl-radio-group', 'Unit - component: sl-radio-group', {
 });
 
 test( 'The disabled state applies the disabled attribute and class', function( assert ) {
-    var component  = this.subject({ disabled: true, name: 'test' }),
-        $component = this.render();
+    var $component;
+
+    this.subject({ disabled: true, name: 'test' });
+    $component = this.render();
 
     assert.ok( $component.prop( 'disabled' ), 'has "disabled" attribute' );
     assert.ok( $component.hasClass( 'disabled' ), 'has "disabled" class' );
 });
 
 test( 'The disabled state applies to sl-radio children', function( assert ) {
-    var component = this.subject({
-            disabled : true,
-            name     : 'test',
+    var $component;
 
-            template: Ember.Handlebars.compile(
-                '{{sl-radio label="One" value="one"}}' +
-                '{{sl-radio label="Two" value="two"}}' +
-                '{{sl-radio label="Three" value="three"}}'
-            )
-        }),
-        $component = this.render();
+    this.subject({
+        disabled : true,
+        name     : 'test',
+
+        template: Ember.Handlebars.compile(
+            '{{sl-radio label="One" value="one"}}' +
+            '{{sl-radio label="Two" value="two"}}' +
+            '{{sl-radio label="Three" value="three"}}'
+        )
+    });
+    $component = this.render();
 
     assert.equal( $component.find( '.sl-radio.disabled' ).length, 3 );
     assert.equal( $component.find( '.sl-radio input[disabled]' ).length, 3 );
 });
 
 test( 'The readonly state applies to sl-radio children', function( assert ) {
-    var component = this.subject({
-            name     : 'test',
-            readonly : true,
+    var $component;
 
-            template: Ember.Handlebars.compile(
-                '{{sl-radio label="One" value="one"}}' +
-                '{{sl-radio label="Two" value="two"}}' +
-                '{{sl-radio label="Three" value="three"}}'
-            )
-        }),
-        $component = this.render();
+    this.subject({
+        name     : 'test',
+        readonly : true,
+
+        template: Ember.Handlebars.compile(
+            '{{sl-radio label="One" value="one"}}' +
+            '{{sl-radio label="Two" value="two"}}' +
+            '{{sl-radio label="Three" value="three"}}'
+        )
+    });
+    $component = this.render();
 
     assert.equal( $component.find( '.sl-radio.readonly' ).length, 3 );
     assert.equal( $component.find( '.sl-radio input[readonly]' ).length, 3 );
@@ -65,34 +71,38 @@ test( 'Value changes when sl-radio child selected', function( assert ) {
 });
 
 test( "Inline true sets sl-radio children's inline property to true", function( assert ) {
-    var component = this.subject({
-            inline : true,
-            name   : 'test',
+    var $component;
 
-            template: Ember.Handlebars.compile(
-                '{{sl-radio label="One" value="one"}}' +
-                '{{sl-radio label="Two" value="two"}}' +
-                '{{sl-radio label="Three" value="three"}}'
-            )
-        }),
-        $component = this.render();
+    this.subject({
+        inline : true,
+        name   : 'test',
+
+        template: Ember.Handlebars.compile(
+            '{{sl-radio label="One" value="one"}}' +
+            '{{sl-radio label="Two" value="two"}}' +
+            '{{sl-radio label="Three" value="three"}}'
+        )
+    });
+    $component = this.render();
 
     assert.equal( $component.find( '.sl-radio.radio' ).length, 0 );
     assert.equal( $component.find( '.sl-radio.radio-inline' ).length, 3 );
 });
 
 test( "Inline false sets sl-radio children's inline property to false", function( assert ) {
-    var component = this.subject({
-            inline : false,
-            name   : 'test',
+    var $component;
 
-            template: Ember.Handlebars.compile(
-                '{{sl-radio inline=true label="One" value="one"}}' +
-                '{{sl-radio inline=true label="Two" value="two"}}' +
-                '{{sl-radio inline=true label="Three" value="three"}}'
-            )
-        }),
-        $component = this.render();
+    this.subject({
+        inline : false,
+        name   : 'test',
+
+        template: Ember.Handlebars.compile(
+            '{{sl-radio inline=true label="One" value="one"}}' +
+            '{{sl-radio inline=true label="Two" value="two"}}' +
+            '{{sl-radio inline=true label="Three" value="three"}}'
+        )
+    });
+    $component = this.render();
 
     assert.equal( $component.find( '.sl-radio.radio-inline' ).length, 0 );
     assert.equal( $component.find( '.sl-radio.radio' ).length, 3 );
