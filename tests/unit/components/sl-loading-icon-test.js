d5fbab73833cd55b110d26e81960f84c7965bd48
Joshua Forisha <jforisha@softlayer.com>
2015-03-11 10:21:47 -0500

    Remove unnecessary var assignments

--- tests/unit/components/sl-loading-icon-test.js
+++ tests/unit/components/sl-loading-icon-test.js
@@ -4,16 +4,20 @@ import { moduleForComponent, test } from 'ember-qunit';
 moduleForComponent( 'sl-loading-icon', 'Unit - component: sl-loading-icon' );
 
 test( 'Default classes are set', function( assert ) {
-    var component  = this.subject(),
-        $component = this.render();
+    var $component;
+
+    this.subject();
+    $component = this.render();
 
     assert.ok( $component.hasClass( 'sl-loading-icon' ), 'Has class "sl-loading-icon"' );
     assert.ok( $component.hasClass( 'sl-loading-icon-dark' ), 'Has class "sl-loading-icon-dark"' );
 });
 
 test( 'Inverse property uses light icon scheme', function( assert ) {
-    var component  = this.subject({ inverse: true }),
-        $component = this.render();
+    var $component;
+
+    this.subject({ inverse: true });
+    $component = this.render();
 
     assert.ok( $component.hasClass( 'sl-loading-icon-light' ), 'Has class "sl-loading-icon-light"' );
 });
