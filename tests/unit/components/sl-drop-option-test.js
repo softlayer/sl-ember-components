d6346af93e22ddf63124b1c4272c243b2c3cbbbf
Joshua Forisha <jforisha@softlayer.com>
2015-03-20 09:11:32 -0500

    Improve drop-option unit tests
    - Removed unnecessary "var component" assignment
    - Added counter assertions for class tests

--- tests/unit/components/sl-drop-option-test.js
+++ tests/unit/components/sl-drop-option-test.js
@@ -26,29 +26,45 @@ test( 'Option type class value depends on `label` value', function( assert ) {
     var component  = this.subject(),
         $component = this.render();
 
-    assert.ok( $component.hasClass( 'divider' ), 'Initially has "divider"' );
+    assert.strictEqual(
+        $component.hasClass( 'presentation' ),
+        false,
+        'Rendered component initially does not have class "presentation"'
+    );
+    assert.ok(
+        $component.hasClass( 'divider' ),
+        'Rendered component initially has class "divider"'
+    );
 
     Ember.run( function() {
         component.set( 'label', 'Test' );
     });
 
+    assert.strictEqual(
+        $component.hasClass( 'divider' ),
+        false,
+        'Rendered component does not have class "divider"'
+    );
+
     assert.ok(
         $component.hasClass( 'presentation' ),
-        'Has class "presentation" with valid "label" value'
+        'Rendered compnonet has class "presentation" with valid "label" value'
     );
 });
 
 test( 'Click triggers bound action', function( assert ) {
-    var component = this.subject({
-            action: 'test',
-            label: 'Test',
-            targetObject: {
-                test: function() {
-                    assert.ok( true, 'Test action fired correctly' );
-                }
+    var $component;
+
+    this.subject({
+        action: 'test',
+        label: 'Test',
+        targetObject: {
+            test: function() {
+                assert.ok( true, 'Test action fired correctly' );
             }
-        }),
-        $component = this.render();
+        }
+    });
+    $component = this.render();
 
     assert.expect( 1 );
     $component.find( 'a' ).trigger( 'click' );
