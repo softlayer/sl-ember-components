1d27880bd20fd886ad994969abfd93d2dba656a2
Joshua Forisha <jforisha@softlayer.com>
2015-06-08 14:15:46 -0500

    Update and sync bower package versions

--- blueprints/sl-ember-components/index.js
+++ blueprints/sl-ember-components/index.js
@@ -4,22 +4,22 @@ module.exports = {
 
         return this.addBowerPackageToProject( 'bootstrap-datepicker', '~1.3.0' )
             .then( function() {
-                return self.addBowerPackageToProject( 'fontawesome', '~4.2.0' );
+                return self.addBowerPackageToProject( 'fontawesome', '~4.3.0' );
             })
             .then( function() {
-                return self.addBowerPackageToProject( 'highcharts', '~4.0.4' );
+                return self.addBowerPackageToProject( 'highcharts', '~4.1.5' );
             })
             .then( function() {
-                return self.addBowerPackageToProject( 'moment', '~2.8.4' );
+                return self.addBowerPackageToProject( 'moment', '~2.10.3' );
             })
             .then( function() {
-                return self.addBowerPackageToProject( 'moment-timezone', '~0.2.5' );
+                return self.addBowerPackageToProject( 'moment-timezone', '~0.4.0' );
             })
             .then( function() {
                 return self.addBowerPackageToProject( 'select2', '~3.5.2' );
             })
             .then( function() {
-                return self.addBowerPackageToProject( 'typeahead.js', '~0.10.5' );
+                return self.addBowerPackageToProject( 'typeahead.js', '~0.11.1' );
             })
             .then( function() {
                 return self.addBowerPackageToProject( 'softlayer/sl-bootstrap', '1.1.0' );
