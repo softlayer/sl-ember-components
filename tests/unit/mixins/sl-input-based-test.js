1c9609e0ff32188ff53d51098a2c597edacc9746
Alex Yu <ayyu@us.ibm.com>
2015-07-10 16:05:20 -0500

    #240 Add test for classNameBindings and remove unnecessary tests

--- tests/unit/mixins/sl-input-based-test.js
+++ tests/unit/mixins/sl-input-based-test.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 import mixinUnderTest from 'sl-ember-components/mixins/sl-input-based';
+import layout from 'sl-ember-components/templates/components/sl-input';
+
 
 module( 'Unit | Mixin | sl input based' );
 
@@ -10,7 +12,7 @@ test( 'Successfully mixed', function( assert ) {
     assert.ok( subject );
 });
 
-test( 'Disabled, optional, readonly, and required are false by default', function( assert ) {
+test( 'Standard values are false by default', function( assert ) {
     let testObject = Ember.Object.extend( mixinUnderTest );
     let subject = testObject.create();
 
@@ -39,162 +41,31 @@ test( 'Disabled, optional, readonly, and required are false by default', functio
     );
 });
 
-test( 'Disabled is passed as a parameter', function( assert ) {
+test( 'Class name bindings contain expected bindings', function( assert ) {
+    assert.expect( 5 );
+
+    let expectedClassNameBindings = [
+        'disabled',
+        'optional',
+        'readonly',
+        'required'
+    ];
     let testObject = Ember.Object.extend( mixinUnderTest );
-    let subject = testObject.create({
-        disabled: true
+    let subject = testObject.create();
+    let classNameBindings = subject.get( 'classNameBindings' );
+
+    assert.equal(
+        classNameBindings.length,
+        expectedClassNameBindings.length,
+        'classNameBindings contains the correct number of elements'
+    );
+
+    classNameBindings.every( ( element, index ) => {
+        assert.strictEqual(
+            element,
+            expectedClassNameBindings[index],
+            `${element} was correctly contained within classNameBindings`
+        );
+        return true;
     });
-
-    assert.strictEqual(
-        subject.get( 'disabled' ),
-        true,
-        'Disabled was properly passed as a parameter'
-    );
-
-    assert.strictEqual(
-        subject.get( 'optional' ),
-        false,
-        'Optional is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'readonly' ),
-        false,
-        'Readonly is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'required' ),
-        false,
-        'Required is false by default'
-    );
-});
-
-
-test( 'Optional is passed as a parameter', function( assert ) {
-    let testObject = Ember.Object.extend( mixinUnderTest );
-    let subject = testObject.create({
-        optional: true
-    });
-
-    assert.strictEqual(
-        subject.get( 'optional' ),
-        true,
-        'Optional was properly passed as a parameter'
-    );
-
-    assert.strictEqual(
-        subject.get( 'disabled' ),
-        false,
-        'Disabled is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'readonly' ),
-        false,
-        'Readonly is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'required' ),
-        false,
-        'Required is false by default'
-    );
-});
-
-
-test( 'Readonly is passed as a parameter', function( assert ) {
-    let testObject = Ember.Object.extend( mixinUnderTest );
-    let subject = testObject.create({
-        readonly: true
-    });
-
-    assert.strictEqual(
-        subject.get( 'readonly' ),
-        true,
-        'Readonly was properly passed as a parameter'
-    );
-
-    assert.strictEqual(
-        subject.get( 'disabled' ),
-        false,
-        'Disabled is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'optional' ),
-        false,
-        'Optional is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'required' ),
-        false,
-        'Required is false by default'
-    );
-});
-
-test( 'Required is passed as a parameter', function( assert ) {
-    let testObject = Ember.Object.extend( mixinUnderTest );
-    let subject = testObject.create({
-        required: true
-    });
-
-    assert.strictEqual(
-        subject.get( 'required' ),
-        true,
-        'Required was properly passed as a parameter'
-    );
-
-    assert.strictEqual(
-        subject.get( 'disabled' ),
-        false,
-        'Disabled is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'optional' ),
-        false,
-        'Optional is false by default'
-    );
-
-    assert.strictEqual(
-        subject.get( 'readonly' ),
-        false,
-        'Readonly is false by default'
-    );
-});
-
-test( 'All parameters can be passed', function( assert ) {
-    let testObject = Ember.Object.extend( mixinUnderTest );
-    let subject = testObject.create({
-        disabled: true,
-        optional: true,
-        readonly: true,
-        required: true
-    });
-
-    assert.strictEqual(
-        subject.get( 'disabled' ),
-        true,
-        'Disabled was properly passed as a parameter'
-    );
-
-    assert.strictEqual(
-        subject.get( 'optional' ),
-        true,
-        'Optional was properly passed as a parameter'
-    );
-
-    assert.strictEqual(
-        subject.get( 'readonly' ),
-        true,
-        'Readonly was properly passed as a parameter'
-    );
-
-    assert.strictEqual(
-        subject.get( 'required' ),
-        true,
-        'Required was properly passed as a parameter'
-    );
 });
