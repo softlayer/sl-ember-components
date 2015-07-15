48e87fecba387d7f731fc4d87bed02f16a409579
Eric Angeles <erangeles@softlayer.com>
2015-07-15 14:28:59 -0500

    got rid of apostrophes in init doc block

--- addon/components/sl-chart.js
+++ addon/components/sl-chart.js
@@ -80,8 +80,8 @@ export default Ember.Component.extend({
      * Check passed parameters on initialization
      *
      * @function
-     * @throws {ember.assert} 'Series property must be an array'
-     * @throws {ember.assert} 'Options property must be an Object'
+     * @throws {ember.assert} Series property must be an Array
+     * @throws {ember.assert} Options property must be an Object
      * @returns {undefined}
      */
     initialize: Ember.on(
