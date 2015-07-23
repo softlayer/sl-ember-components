d6e0b4ee170ccfba80a3605129ec011334767118
Eric Angeles <erangeles@softlayer.com>
2015-07-23 17:08:12 -0500

    Closes softlayer/sl-ember-components#342

--- blueprints/sl-ember-components/index.js
+++ blueprints/sl-ember-components/index.js
@@ -3,14 +3,31 @@ module.exports = {
         const self = this;
 
         return this.addBowerPackagesToProject([
-            {name: 'bootstrap-datepicker', target: '~1.3.0'},
-            {name: 'fontawesome', target: '~4.3.0'},
-            {name: 'highcharts', target: '~4.1.5'},
-            {name: 'moment', target: '~2.10.3'},
-            {name: 'moment-timezone', target: '~0.4.0'},
-            {name: 'select2', target: '~3.5.2'},
-            {name: 'typeahead.js', target: '~0.11.1'},
-            {name: 'softlayer/sl-bootstrap', target: '1.1.0'}
+            {
+                name: 'bootstrap-datepicker',
+                target: '~1.3.0'
+            }, {
+                name: 'fontawesome',
+                target: '~4.3.0'
+            }, {
+                name: 'highcharts',
+                target: '~4.1.5'
+            }, {
+                name: 'moment',
+                target: '~2.10.3'
+            }, {
+                name: 'moment-timezone',
+                target: '~0.4.0'
+            }, {
+                name: 'select2',
+                target: '~3.5.2'
+            }, {
+                name: 'typeahead.js',
+                target: '~0.11.1'
+            }, {
+                name: 'softlayer/sl-bootstrap',
+                target: '1.1.0'
+            }
         ]);
     },
 
