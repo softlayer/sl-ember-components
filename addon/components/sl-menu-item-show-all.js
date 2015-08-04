711fa85b027f99cb93b196266dd0075dcef1d0f4
Joshua Forisha <jforisha@softlayer.com>
2015-08-04 15:40:55 -0500

    Added missing docblock

--- addon/components/sl-menu-item-show-all.js
+++ addon/components/sl-menu-item-show-all.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 import SlMenuItem from './sl-menu-item';
 import layout from '../templates/components/sl-menu-item-show-all';
 
+/**
+ * @module
+ * @augments module:components/sl-button
+ */
 export default SlMenuItem.extend({
 
     // -------------------------------------------------------------------------
