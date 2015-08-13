bb3d2cc7d2409d489222decacf7f0b2c9fe7610b
Aziz Punjani <aziz.punjani@gmail.com>
2015-08-13 14:43:41 -0500

    Use Controller instead of ArrayController.
    Closes #551

--- tests/dummy/app/controllers/demos/sl-calendar.js
+++ tests/dummy/app/controllers/demos/sl-calendar.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.ArrayController.extend({
+export default Ember.Controller.extend({
     actions: {
         logLabel( dateObjects ) {
             window.console.log( dateObjects[ 0 ] );
