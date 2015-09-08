f0c9960dadfe72936d268b3fa6e253a74c12e089
Aziz Punjani <aziz.punjani@gmail.com>
2015-09-08 09:56:22 -0500

    Changed class selectors to immediate first child selector.

--- tests/integration/components/sl-span-test.js
+++ tests/integration/components/sl-span-test.js
@@ -24,7 +24,7 @@ test( 'If "loading" is true, sl-loading-icon component is displayed', function( 
     ` );
 
     assert.equal(
-        this.$( '.sl-loading-icon' ).length,
+        this.$( '>:first-child' ).find( '.sl-loading-icon' ).length,
         0,
         'Loading icon is not present initially'
     );
@@ -35,7 +35,7 @@ test( 'If "loading" is true, sl-loading-icon component is displayed', function( 
     ` );
 
     assert.equal(
-        this.$( '.sl-loading-icon' ).length,
+        this.$( '>:first-child' ).find( '.sl-loading-icon' ).length,
         1,
         'Loading icon is present while span is loading'
     );
@@ -47,8 +47,9 @@ test( 'Inverse property applies to loading-icon', function( assert ) {
         {{/sl-span}}
     ` );
 
-    assert.ok(
-        this.$( '.sl-loading-icon' ).hasClass( 'sl-loading-icon-dark' ),
+    assert.equal(
+        this.$( '>:first-child' ).find( '.sl-loading-icon-dark' ).length,
+        1,
         'Loading icon is dark initially'
     );
 
@@ -57,8 +58,9 @@ test( 'Inverse property applies to loading-icon', function( assert ) {
         {{/sl-span}}
     ` );
 
-    assert.ok(
-        this.$( '.sl-loading-icon' ).hasClass( 'sl-loading-icon-light' ),
+    assert.equal(
+        this.$( '>:first-child' ).find( '.sl-loading-icon-light' ).length,
+        1,
         'Loading icon is light when inverse'
     );
 });
