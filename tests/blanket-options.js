e7b8b61c23e62a2eff49834063c1f10c0f5a04d3
Jeremy Brown <jebrown@softlayer.com>
2015-09-18 15:00:50 -0500

    Comply with linting

--- tests/blanket-options.js
+++ tests/blanket-options.js
@@ -1,20 +1,21 @@
 /* globals blanket, module */
 
-var options = {
-  modulePrefix: 'sl-ember-components',
-  filter: '//.*sl-ember-components/.*/',
-  antifilter: '//.*(tests|template).*/',
-  loaderExclusions: [],
-  enableCoverage: true,
-  modulePattern: "\/(\\w+)",
-  branchTracking: true,
-  cliOptions: {
-    reporters: ['json'],
-    autostart: true
-  }
+const options = {
+    modulePrefix: 'sl-ember-components',
+    filter: '//.*sl-ember-components/.*/',
+    antifilter: '//.*(tests|template).*/',
+    loaderExclusions: [],
+    enableCoverage: true,
+    modulePattern: '\/(\\w+)',
+    branchTracking: true,
+    cliOptions: {
+        reporters: [ 'json' ],
+        autostart: true
+    }
 };
-if (typeof exports === 'undefined') {
-  blanket.options(options);
+
+if ( 'undefined' === typeof exports ) {
+    blanket.options( options );
 } else {
-  module.exports = options;
+    module.exports = options;
 }
