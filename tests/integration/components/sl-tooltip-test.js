8078317f5633ab453afa92086202439713138958
eangele@us.ibm.com <erangeles@softlayer.com>
2015-09-30 21:11:26 -0500

    Added more test per pr review

--- tests/integration/components/sl-tooltip-test.js
+++ tests/integration/components/sl-tooltip-test.js
@@ -31,3 +31,32 @@ test( 'Renders as a span tag with no classes', function( assert ) {
         'Default class of ember-view renders successfully'
     );
 });
+
+test( '"Title" capabilities are supported', function( assert ) {
+    this.set( 'title', 'test title' );
+
+    this.render( hbs`
+        {{sl-tooltip title=title}}
+    ` );
+
+    assert.strictEqual(
+        this.$( '>:first-child' ).attr( 'data-original-title' ),
+        this.get( 'title' ),
+        'Title prop supported'
+    );
+});
+
+test( '"Popover" capabilities are supported', function( assert ) {
+    this.set( 'popover', 'Popover content' );
+    this.set( 'title', 'test title' );
+
+    this.render( hbs`
+        {{sl-tooltip title=title popover=popover}}
+    ` );
+
+    assert.strictEqual(
+        this.$( '>:first-child' ).data( 'bs.popover' ).options.content,
+        this.get( 'popover' ),
+        'Popover capabilites are supported'
+    );
+});
