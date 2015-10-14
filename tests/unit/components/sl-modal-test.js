e39f201ebb8ef5fffd3f311af91956baa6e0d37f
jecorley <jecorley@softlayer.com>
2015-10-14 10:26:12 -0500

    created test file

---
+++ tests/unit/components/sl-modal-test.js
@@ -0,0 +1,7 @@
+import Ember from 'ember';
+import { moduleForComponent, test } from 'ember-qunit';
+import sinon from 'sinon';
+
+moduleForComponent( 'sl-modal', 'Unit | Component | sl modal', {
+    unit: true
+});
