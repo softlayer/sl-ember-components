26ede313c9d845fd98f0147157abd6b4ab341217
jecorley <jfcorley+github@gmail.com>
2015-10-14 14:47:56 -0500

    fixed the generated code to pass our lint

--- tests/unit/components/sl-modal-header-test.js
+++ tests/unit/components/sl-modal-header-test.js
@@ -1,6 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 
-moduleForComponent('sl-modal-header', 'Unit | Component | sl modal header', {
+moduleForComponent( 'sl-modal-header', 'Unit | Component | sl modal header', {
     unit: true
 });
 
