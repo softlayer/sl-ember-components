c68fb95f258ee4310b664672c2231ef6fbb4f3d7
jecorley <jecorley@softlayer.com>
2015-10-14 14:25:49 -0500

    added default property test

--- tests/unit/components/sl-modal-header-test.js
+++ tests/unit/components/sl-modal-header-test.js
@@ -3,3 +3,13 @@ import { moduleForComponent, test } from 'ember-qunit';
 moduleForComponent('sl-modal-header', 'Unit | Component | sl modal header', {
     unit: true
 });
+
+test( 'Default property values are set correctly', function( assert ) {
+    const component = this.subject();
+
+    assert.strictEqual(
+        component.get( 'tagName' ),
+        'div',
+        'Default tabName is "div"'
+    );
+});
