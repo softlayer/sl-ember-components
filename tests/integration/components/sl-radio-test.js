bb61ae1e27e7b2e51725a877950b3ff205ea4873
Michael Carroll <juwara0@users.noreply.github.com>
2015-11-05 10:49:08 -0600

    Closes softlayer/sl-ember-components#769

--- tests/integration/components/sl-radio-test.js
+++ tests/integration/components/sl-radio-test.js
@@ -5,6 +5,27 @@ moduleForComponent( 'sl-radio', 'Integration | Component | sl radio', {
     integration: true
 });
 
+test( 'Default rendered state', function( assert ) {
+    this.render( hbs`
+        {{sl-radio}}
+    ` );
+
+    assert.ok(
+        this.$( '>:first-child' ).hasClass( 'sl-radio' ),
+        'Has class "sl-radio"'
+    );
+
+    assert.ok(
+        this.$( '>:first-child' ).hasClass( 'radio' ),
+        'Has class "radio"'
+    );
+
+    assert.ok(
+        this.$( '>:first-child' ).find( 'input[type="radio"]' ),
+        'input type is set to "radio"'
+    );
+});
+
 test( 'Disabled state applies disabled class, and attribute to input', function( assert ) {
     this.render( hbs`
         {{sl-radio disabled=true}}
@@ -51,5 +72,33 @@ test( 'name applies property to input', function( assert ) {
         this.$( '>:first-child' ).find( 'input' ).prop( 'name' ),
         'testname',
         'Rendered input has name set'
+    );
+});
+
+test( '"value" property is supported', function( assert ) {
+    this.set( 'valueTest', 'testValue' );
+
+    this.render( hbs`
+        {{sl-radio value=valueTest}}
+    ` );
+
+    assert.strictEqual(
+        this.$( '>:first-child' ).find( 'input' ).val(),
+        'testValue',
+        'input value gets set'
+    );
+});
+
+test( '"label" property is supported', function( assert ) {
+    this.set( 'labelTest', 'testLabel' );
+
+    this.render( hbs`
+        {{sl-radio label=labelTest}}
+    ` );
+
+    assert.strictEqual(
+        this.$( '>:first-child' ).find( 'label' ).text().trim(),
+        'testLabel',
+        '"label" value gets set'
     );
 });
