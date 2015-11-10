677291f01543e42e09f5e0c45b9a72c679862c13
Aziz Punjani <aziz.punjani@gmail.com>
2015-11-10 14:49:18 -0600

    Added integration tests for sl-grid-cell

--- tests/unit/components/sl-grid-cell-test.js
+++ tests/unit/components/sl-grid-cell-test.js
@@ -20,8 +20,7 @@ const ColumnSize = Object.freeze({
     SMALL: 'small'
 });
 
-const defaultColumn = { valuePath: 'value' };
-const defaultRow = { value: 'Test' };
+const defaultColumn = Ember.Object.extend();
 
 test( 'Default property values', function( assert ) {
     const component = this.subject();
@@ -45,8 +44,43 @@ test( 'Default property values', function( assert ) {
     );
 });
 
+test( 'Dependent keys are correct', function( assert ) {
+    const component = this.subject();
+
+    const alignmentClassDependentKeys = [
+        'column.align'
+    ];
+
+    const contentValueDependentKeys = [
+        'column',
+        'row'
+    ];
+
+    const sizeClassDependentKeys = [
+        'column.size'
+    ];
+
+    assert.deepEqual(
+        component.alignmentClass._dependentKeys,
+        alignmentClassDependentKeys,
+        'Dependent keys are correct for alignmentClass()'
+    );
+
+    assert.deepEqual(
+        component.contentValue._dependentKeys,
+        contentValueDependentKeys,
+        'Dependent keys are correct for contentValue()'
+    );
+
+    assert.deepEqual(
+        component.sizeClass._dependentKeys,
+        sizeClassDependentKeys,
+        'Dependent keys are correct for sizeClass()'
+    );
+});
+
 test( 'alignmentClass() returns correct alignment value', function( assert ) {
-    const column = Ember.Object.extend().create();
+    const column = defaultColumn.create();
     const component = this.subject({
         column: column
     });
@@ -91,7 +125,7 @@ test( 'alignmentClass() returns correct alignment value', function( assert ) {
 });
 
 test( 'sizeClass() returns correct size value', function( assert ) {
-    const column = Ember.Object.extend().create();
+    const column = defaultColumn.create();
     const component = this.subject({
         column: column
     });
@@ -130,25 +164,69 @@ test( 'sizeClass() returns correct size value', function( assert ) {
     utils.warn.restore();
 });
 
-
-test( 'Column alignment class is applied', function( assert ) {
-    const column = Object.create( defaultColumn );
-
-    this.subject({ column, row: defaultRow });
-
-    assert.equal(
-        this.$().hasClass( 'text-right' ),
-        false,
-        'Default component does not have right-alignment class'
-    );
-
-    Ember.run( () => {
-        Ember.set( column, 'align', 'right' );
+test( 'style() returns the correct value', function( assert ) {
+    const column = defaultColumn.create();
+    const component = this.subject({
+        column: column
     });
 
-    assert.ok(
-        this.$().hasClass( 'text-right' ),
-        'Component has expected class "text-right" with right-aligned column'
+    assert.strictEqual(
+        component.get( 'style' ).string,
+        '',
+        'style() is an empty string when column size is not set'
+    );
+
+    column.set( 'size', 100 );
+
+    assert.strictEqual(
+        component.get( 'style' ).string,
+        'width: 100px;',
+        'style() returns a correct style value when a number is set'
+    );
+
+    column.set( 'size', 'notNumber' );
+
+    assert.strictEqual(
+        component.get( 'style' ).string,
+        '',
+        'style() returns an empty string when size is not a number'
+    );
+});
+
+test( 'contentValue() returns the correct value', function( assert ) {
+    const column = defaultColumn.create({
+        valuePath: 'name'
+    });
+
+    let row = Ember.Object.extend().create({
+        name: 'test'
+    });
+
+    const component = this.subject({
+        column,
+        row
+    });
+
+    assert.strictEqual(
+        component.get( 'contentValue' ),
+        row.name,
+        'contentValue() returned correct result for row'
+    );
+
+    row = Ember.Object.extend().create({
+        model: {
+            name: 'anotherTest'
+        }
+    });
+
+    Ember.run( () => {
+        component.set( 'row', row );
+    });
+
+    assert.strictEqual(
+        component.get( 'contentValue' ),
+        row.get( 'model.name' ),
+        'contentValue() returns model data when row model is set'
     );
 });
 
@@ -166,65 +244,10 @@ test( 'ColumnSize and ColumnAlign values are correct', function( assert ) {
     );
 });
 
-test( 'Primary column class is applied', function( assert ) {
-    const column = Object.create( defaultColumn );
-
-    this.subject({ column, row: defaultRow });
-
-    assert.equal(
-        this.$().hasClass( 'primary-column' ),
-        false,
-        'Default component does not have primary column class'
-    );
-
-    Ember.run( () => {
-        Ember.set( column, 'primary', true );
-    });
-
-    assert.ok(
-        this.$().hasClass( 'primary-column' ),
-        'Component has expected class "primary-column" with primary' +
-        ' column definition'
-    );
-});
-
-test( 'Column size values are supported', function( assert ) {
-    const column = Object.create( defaultColumn );
-    const component = this.subject({ column, row: defaultRow });
-
-    assert.equal(
-        Ember.typeOf( component.get( 'sizeClass' ) ),
-        'null',
-        'Default component sizeClass value is null'
-    );
-
-    assert.equal(
-        this.$().hasClass( 'column-small' ),
-        false,
-        'Default component does not have "column-small" class'
-    );
-
-    Ember.run( () => {
-        Ember.set( column, 'size', 42 );
-    });
-
-    assert.equal(
-        this.$().width(),
-        42,
-        'Setting column size to a number is supported'
-    );
-
-    Ember.run( () => {
-        Ember.set( column, 'size', 'small' );
-    });
-
-    assert.ok(
-        this.$().hasClass( 'column-small' ),
-        'Setting column size to a valid string value is supported'
-    );
-});
-
 test( 'Click event action is supported', function( assert ) {
+    const defaultColumn = { valuePath: 'value' };
+    const defaultRow = { value: 'Test' };
+
     this.subject({
         column: defaultColumn,
         onClick: 'test',
@@ -242,14 +265,4 @@ test( 'Click event action is supported', function( assert ) {
     });
 
     this.$().trigger( 'click' );
-});
-
-test( 'Content value is handled for valuePath', function( assert ) {
-    this.subject({ column: defaultColumn, row: defaultRow });
-
-    assert.equal(
-        Ember.$.trim( this.$().text() ),
-        defaultRow.value,
-        'Component content is expected value'
-    );
 });
