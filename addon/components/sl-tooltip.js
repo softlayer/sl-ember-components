057022bac619ec7cbeba2d16622f41f8177df6bc
Yogeswar Vijayakumar <yvijayakumar@softlayer.com>
2015-11-10 13:09:45 -0600

    update code styling

--- addon/components/sl-tooltip.js
+++ addon/components/sl-tooltip.js
@@ -41,7 +41,8 @@ export default Ember.Component.extend( TooltipEnabled, {
         'init',
         function() {
             if ( Ember.typeOf( this.get( 'title' ) ) !== 'string' ) {
-                throw new Ember.Error( 'enableTooltip() and enablePopover() expect the parameter "title" and for it to be a string' );
+                throw new Ember.Error( 'enableTooltip() and enablePopover()' +
+                            ' expect the parameter "title" and for it to be a string' );
             }
 
             if ( Ember.typeOf( this.get( 'popover' ) ) !== 'string' &&
