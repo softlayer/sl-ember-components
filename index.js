17b6efc92d25647a3286205ddf7c41d7c85ffc5e
Jeremy Brown <jebrown@softlayer.com>
2015-11-11 14:18:35 -0600

    Change reference to "bower_components" path from hard-coded to dynamic

--- index.js
+++ index.js
@@ -11,45 +11,45 @@ module.exports = {
         this._super.included( app );
 
         app.import({
-            development: 'bower_components/bootstrap/dist/js/bootstrap.js',
-            production: 'bower_components/bootstrap/dist/js/bootstrap.min.js'
+            development: app.bowerDirectory + '/bootstrap/dist/js/bootstrap.js',
+            production: app.bowerDirectory + '/bootstrap/dist/js/bootstrap.min.js'
         });
 
-        app.import( 'bower_components/bootstrap-datepicker/js/bootstrap-datepicker.js' );
+        app.import( app.bowerDirectory + '/bootstrap-datepicker/js/bootstrap-datepicker.js' );
 
         app.import({
-            development: 'bower_components/highcharts/highcharts.src.js',
-            production: 'bower_components/highcharts/highcharts.js'
-        });
-
-        app.import({
-            development: 'bower_components/jquery-mousewheel/jquery.mousewheel.js',
-            production: 'bower_components/jquery-mousewheel/jquery.mousewheel.min.js'
+            development: app.bowerDirectory + '/highcharts/highcharts.src.js',
+            production: app.bowerDirectory + '/highcharts/highcharts.js'
         });
 
         app.import({
-            development: 'bower_components/moment/min/moment-with-locales.js',
-            production: 'bower_components/moment/min/moment-with-locales.min.js'
+            development: app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.js',
+            production: app.bowerDirectory + '/jquery-mousewheel/jquery.mousewheel.min.js'
         });
 
         app.import({
-            development: 'bower_components/moment-timezone/builds/moment-timezone-with-data.js',
-            production: 'bower_components/moment-timezone/builds/moment-timezone-with-data.min.js'
+            development: app.bowerDirectory + '/moment/min/moment-with-locales.js',
+            production: app.bowerDirectory + '/moment/min/moment-with-locales.min.js'
         });
 
         app.import({
-            development: 'bower_components/rxjs/dist/rx.all.js',
-            production: 'bower_components/rxjs/dist/rx.all.min.js'
+            development: app.bowerDirectory + '/moment-timezone/builds/moment-timezone-with-data.js',
+            production: app.bowerDirectory + '/moment-timezone/builds/moment-timezone-with-data.min.js'
         });
 
         app.import({
-            development: 'bower_components/select2/select2.js',
-            production: 'bower_components/select2/select2.min.js'
+            development: app.bowerDirectory + '/rxjs/dist/rx.all.js',
+            production: app.bowerDirectory + '/rxjs/dist/rx.all.min.js'
         });
 
         app.import({
-            development: 'bower_components/typeahead.js/dist/typeahead.bundle.js',
-            production: 'bower_components/typeahead.js/dist/typeahead.bundle.min.js'
+            development: app.bowerDirectory + '/select2/select2.js',
+            production: app.bowerDirectory + '/select2/select2.min.js'
+        });
+
+        app.import({
+            development: app.bowerDirectory + '/typeahead.js/dist/typeahead.bundle.js',
+            production: app.bowerDirectory + '/typeahead.js/dist/typeahead.bundle.min.js'
         });
     },
 
