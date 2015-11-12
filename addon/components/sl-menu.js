cdf3174c8d3ccfc8010c490f00622978d7b3fbc0
eangele@us.ibm.com <erangeles@softlayer.com>
2015-11-12 14:35:15 -0600

    Revert "Closes softlayer/sl-ember-components#1099"
    This reverts commit 8d708aba33cabfee12584258a1d8726bf8472df6.

--- addon/components/sl-menu.js
+++ addon/components/sl-menu.js
@@ -203,10 +203,6 @@ export default Ember.Component.extend( StreamEnabled, {
             stream.on( 'showAll', () => {
                 this.showAll();
             });
-
-            stream.on( 'clearSelections', () => {
-                this.clearSelections();
-            });
         }
     ),
 
