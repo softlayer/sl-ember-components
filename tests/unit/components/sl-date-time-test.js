b493a3f6ff8450d41bc930c1591ff1df08e87560
eangele@us.ibm.com <erangeles@softlayer.com>
2015-11-13 14:40:44 -0600

    added type test for timezone

--- tests/unit/components/sl-date-time-test.js
+++ tests/unit/components/sl-date-time-test.js
@@ -202,3 +202,89 @@ test( 'Dependent keys are correct', function( assert ) {
         'Dependent keys are correct for timezoneString()'
     );
 });
+
+test( 'init() - "timezone" property needs to be a string', function( assert ) {
+    const properties = Ember.Object.create();
+
+    const callSubject = () => this.subject( properties );
+
+    // Empty Property
+
+    assert.throws(
+        callSubject,
+        'Property was empty'
+    );
+
+    // Null Property
+
+    properties.set( 'timezone', null );
+
+    assert.throws(
+        callSubject,
+        'Property was null'
+    );
+
+
+    // Number Property
+
+    properties.set( 'timezone', 3 );
+
+    assert.throws(
+        callSubject,
+        'Property was a number'
+    );
+
+    // Boolean Property
+
+    properties.set( 'timezone', true );
+
+    assert.throws(
+        callSubject,
+        'Property was a boolean'
+    );
+
+    // Array Property
+
+    properties.set( 'timezone', [] );
+
+    assert.throws(
+        callSubject,
+        'Property was an array'
+    );
+
+    // Function Property
+
+    properties.set( 'timezone', function() { } );
+
+    assert.throws(
+        callSubject,
+        'Property was a function'
+    );
+
+    // Object Property
+
+    properties.set( 'timezone', {} );
+
+    assert.throws(
+        callSubject,
+        'Property was an object'
+    );
+
+    // Undefined Property
+
+    properties.set( 'timezone', undefined );
+
+    assert.throws(
+        callSubject,
+        'Property was undefined'
+    );
+
+    // String Property
+
+    properties.set( 'timezone', 'Test title' );
+
+    assert.ok(
+        callSubject(),
+        'Property was a string'
+    );
+});
