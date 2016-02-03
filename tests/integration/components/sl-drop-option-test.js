d766779883de6bc51e544135cb760fb17326c520
Michael Carroll <juwara0@users.noreply.github.com>
2016-02-03 08:04:36 -0600

    redined usage, updated template, demo docs and tests

--- tests/integration/components/sl-drop-option-test.js
+++ tests/integration/components/sl-drop-option-test.js
@@ -28,33 +28,6 @@ test( 'ARIA role is set when "label" attribute is set', function( assert ) {
     );
 });
 
-test( 'label is present thus hyperlink tag is rendered', function( assert ) {
-    this.render( hbs`
-        {{sl-drop-option}}
-    ` );
-
-    assert.notOk(
-        this.$( '>:first-child' ).find( 'a' ).attr( 'role' ),
-        'Hyperlink is not rendered if label is not set'
-    );
-
-    this.render( hbs`
-        {{sl-drop-option label="test"}}
-    ` );
-
-    assert.strictEqual(
-        this.$( '>:first-child' ).find( 'a' ).attr( 'role' ),
-        'menuitem',
-        'Hyperlink is rendered if label is set'
-    );
-
-    assert.strictEqual(
-        this.$( '>:first-child' ).find( 'a' ).text().trim(),
-        'test',
-        'Hyperlink text is that of label value'
-    );
-});
-
 test( 'Action is wired into template hyperlink tag', function( assert ) {
     assert.expect( 1 );
 
@@ -74,25 +47,6 @@ test( 'Action is wired into template hyperlink tag', function( assert ) {
     });
 
     this.$( '>:first-child' ).find( 'a' ).click();
-});
-
-test( '"label" attribute can be set with yielded content', function( assert ) {
-    this.render( hbs`
-        {{#sl-drop-option label="test"}}
-            <i class="testClass"></i>
-        {{/sl-drop-option}}
-    ` );
-
-    assert.strictEqual(
-        this.$( '>:first-child' ).find( 'a' ).text().trim(),
-        'test',
-        '"label" attribute value is set'
-    );
-
-    assert.ok(
-        this.$( '>:first-child' ).find( 'i' ).hasClass( 'testClass' ),
-        'Content is yielded correctly'
-    );
 });
 
 test( 'Content is yielded', function( assert ) {
