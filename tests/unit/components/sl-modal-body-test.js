cae6ca46cb2795792df4052f495db1822d247732
Aziz Punjani <aziz.punjani@gmail.com>
2016-02-08 08:36:34 -0600

    Added dual instance tests sl-modal-body

---
+++ tests/unit/components/sl-modal-body-test.js
@@ -0,0 +1,22 @@
+import { moduleForComponent, test } from 'ember-qunit';
+import globalLibraries from '../../helpers/sl/synchronous/global-libraries';
+
+moduleForComponent( 'sl-modal-body', 'Unit | Component | sl modal body', {
+    unit: true
+});
+
+test( 'There are no references to Ember.$, $ or jQuery', function( assert ) {
+    globalLibraries.setupSpies();
+
+    const component = this.subject();
+
+    this.render();
+
+    globalLibraries.triggerEvents( component );
+
+    assert.notOk(
+        globalLibraries.called()
+    );
+
+    globalLibraries.restoreSpies();
+});
